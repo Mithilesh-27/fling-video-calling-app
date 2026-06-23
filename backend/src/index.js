023898f33e4e902ab41baa9ff55212148a7e71db
Mithilesh <mithileshsingpure@gmail.com>
2026-06-23 12:00:15 +0530

    Refactor server setup: organize imports, adjust CORS origin, and enhance production routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 import { connectDB } from './db/connect.db.js';
 connectDB();
 
+import path from "path";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js";
@@ -14,8 +15,11 @@ import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 app.use(cookieParser());
 app.use(express.json());
+
+const __dirname = path.resolve();
+
 app.use(cors({
-  origin: process.env.FRONTEND_DOMAIN || "http://localhost:5173",
+  origin: "http://localhost:5173",
   credentials: true, // Allow cookies to be sent in cross-origin requests
 }));
 
@@ -23,10 +27,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get('/', (req, res) => {
-  res.send("Hello, World!");
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
